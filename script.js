640203a2fba5d67f68478ebc00dbe32523034d21
Syntex14 <rangelabraham14@gmail.com>
2023-05-15 09:47:36 -0500

    Completed basic functions for add, subtract, multiply and divide.

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1. Create functions for simple operations   
+    // add
+    // subtract
+    // multiply
+    // divide
+
+
+function addOperation(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtractOperation(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiplyOperation (numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divideOperation (numOne, numTwo) {
+    return numOne / numTwo;
+}
+
