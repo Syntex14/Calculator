3c69da5b9a8177e60feca95be314b93599164ebf
Syntex14 <rangelabraham14@gmail.com>
2023-05-15 10:05:30 -0500

    Added basic variables for number one, number two, and operator selector. Created operate function which will call each operator when notified by an event listener.

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function divideOperation (numOne, numTwo) {
     return numOne / numTwo;
 }
 
+let numOne;
+let numTwo;
+let operatorSelector;
+
+function operate () { // will be called by an event clicker listener
+    switch (operatorSelector) {
+        case "add":
+            addOperation(numOne, numTwo);
+            break;
+    
+        case "subtract":
+            subtractOperation(numOne, numTwo);
+            break;
+        case "multiply":
+            multiplyOperation(numOne, numTwo);
+            break;
+        case "divide":
+            divideOperation(numOne, numTwo);
+}
+}
